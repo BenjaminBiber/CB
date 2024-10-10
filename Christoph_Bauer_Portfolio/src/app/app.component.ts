f798e6024f71da7a7f1c4ffb47c1068ccc680231
Benjamin Biber <ben.biber.strass@gmail.com>
2024-10-10 13:56:31 +0200

    Added MainPage with Placeholder Image, Typewriter Effect and made it a bit more responsive

--- Christoph_Bauer_Portfolio/src/app/app.component.ts
+++ Christoph_Bauer_Portfolio/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponentComponent } from "../header-component/header-component.component";
 
@@ -9,6 +9,51 @@ import { HeaderComponentComponent } from "../header-component/header-component.c
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Christoph_Bauer_Portfolio';
+  Description: string[] = [' Fotograf', ' Videograf', ' Cutter'];
+  viewText:string = '';
+
+  async ngOnInit() {
+    await this.TextEffect();
+  }
+
+  async TextEffect() {
+    while (true) {
+      for (let desc of this.Description) {
+        for (let txt of desc) {
+          this.viewText += txt;
+          await this.sleep(450);
+        }
+        await this.sleep(1000)
+        while (this.viewText.length > 0) {
+          this.viewText = this.viewText.slice(0, -1);
+          await this.sleep(450);
+        }
+      }
+    }
+  }
+
+
+  sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  transformStyle: string = '';
+
+  onMouseMove(event: MouseEvent) {
+    const imageContainer = event.currentTarget as HTMLElement;
+    const rect = imageContainer.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    const moveX = (x / rect.width - 0.5) * 20;
+    const moveY = (y / rect.height - 0.5) * 20;
+
+    this.transformStyle = `translate(${moveX}px, ${moveY}px) scale(1.0)`;
+  }
+
+  onMouseLeave() {
+    this.transformStyle = 'translate(0, 0) scale(1)';
+  }
 }
--- Christoph_Bauer_Portfolio/src/header-component/header-component.component.ts
+++ Christoph_Bauer_Portfolio/src/header-component/header-component.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from "../Components/ang-button/ang-button.component";
 
 @Component({
   selector: 'app-header-component',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [],
   templateUrl: './header-component.component.html',
   styleUrl: './header-component.component.css'
 })
 export class HeaderComponentComponent {
-
 }
